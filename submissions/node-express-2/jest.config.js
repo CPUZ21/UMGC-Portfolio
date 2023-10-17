871e4c9103d689a273122b1c225dc6c49c57d802
isaiah <itomlin801@gmail.com>
2023-10-17 04:14:22 -0400

    commit

---
+++ submissions/node-express-2/jest.config.js
@@ -0,0 +1,178 @@
+const time = (h, m) => {
+    let count = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine", "thirty"]
+    if (m === 1) {
+      return count[h] + " oh one ";
+    } else
+    if (m === 2) {
+      return count[h] + " oh two ";
+    } else
+    if (m === 3 ) {
+      return count[h] + " oh three"; 
+    } else
+    if (m === 4) {
+      return count[h] + " oh four ";
+    } else
+    if (m === 5) {
+      return count[h] + "oh five";
+    } else 
+    if (m === 6) {
+      return count[h] + "oh six";
+    } else
+    if (m === 7) {
+      return count[h] + " oh seven ";
+    } else 
+    if (m === 8) {
+      return count[h] + " oh eight ";
+    } else
+    if (m === 9) {
+        return count[h] + " oh nine ";
+    } else
+    if (m === 10) {
+        return count[h] + " ten";
+    } else 
+    if (m === 11) {
+        return count[h] + " eleven";
+    } else 
+    if (m === 12) {
+        return count[h] + " twelve ";
+    } else 
+    if (m === 13) {
+        return count[h] + " thirteen ";
+    } else 
+    if (m === 14) {
+        return count[h] + " fourteen ";
+    } else
+    if (m === 15) {
+        return count[h] + " fifteen ";
+    } else
+    if (m === 16) {
+        return count[h] + " sixteen ";
+    } else
+    if (m === 17) {
+        return count[h] + " seventeen ";
+    } else
+    if (m === 18) {
+        return count[h] + " eightteen ";
+    } else
+    if (m === 19) {
+        return count[h] + " nineteen ";
+    } else
+    if (m === 20) {
+        return count[h] + " twenty ";
+    } else
+    if (m === 21) {
+        return count[h] + " twenty one ";
+    } else
+    if (m === 22) {
+        return count[h] + " twenty two ";
+    } else
+    if (m === 23) {
+        return count[h] + " twenty three ";
+    } else
+    if (m === 24) {
+        return count[h] + " twenty four ";
+    } else
+    if (m === 25) {
+        return count[h] + " twenty five ";
+    } else
+    if (m === 26) {
+        return count[h] + " twenty six ";
+    } else
+    if (m === 27) {
+        return count[h] + " twenty seven ";
+    } else
+    if (m === 28) {
+        return count[h] + " twenty eight ";
+    } else
+    if (m === 29) {
+        return count[h] + " twenty nine ";
+    } else
+    if (m === 30) {
+        return count[h] + " thirty ";
+    } else
+    if (m === 31) {
+        return count[h] + " thirty one ";
+    } else
+    if (m === 32) {
+        return count[h] + " thirty two ";
+    } else
+    if (m === 33) {
+        return count[h] + " thirty three ";
+    } else
+    if (m === 34) {
+        return count[h] + " thirty four ";
+    } else
+    if (m === 35) {
+        return count[h] + " thirty five ";
+    } else
+    if (m === 36) {
+        return count[h] + " thirty six ";
+    } else
+    if (m === 37) {
+        return count[h] + " thirty seven ";
+    } else
+    if (m === 38) {
+        return count[h] + " thirty eight ";
+    } else
+    if (m === 39) {
+        return count[h] + " thirty nine ";
+    } else
+    if (m === 41) {
+        return count[h] + " forty one ";
+    } else
+    if (m === 42) {
+        return count[h] + " forty two ";
+    } else
+    if (m === 43) {
+        return count[h] + " forty three ";
+    } else
+    if (m === 44) {
+        return count[h] + " forty four ";
+    } else
+    if (m === 45) {
+        return count[h] + " forty five ";
+    } else
+    if (m === 46) {
+        return count[h] + " forty six ";
+    } else
+    if (m === 47) {
+        return count[h] + " forty seven ";
+    } else
+    if (m === 48) {
+        return count[h] + " forty eight ";
+    } else
+    if (m === 49) {
+        return count[h] + " forty nine ";
+    } else
+    if (m === 50) {
+        return count[h] + " fifty ";
+    } else
+    if (m === 51) {
+        return count[h] + " fifty one ";
+    } else
+    if (m === 52) {
+        return count[h] + " fifty two ";
+    } else
+    if (m === 53) {
+        return count[h] + " fifty three ";
+    } else
+    if (m === 54) {
+        return count[h] + " fifty four ";
+    } else
+    if (m === 55) {
+        return count[h] + " fifty five ";
+    } else
+    if (m === 56) {
+        return count[h] + " fifty six ";
+    } else
+    if (m === 57) {
+        return count[h] + " fifty seven ";
+    } else
+    if (m === 58) {
+        return count[h] + " fifty eight ";
+    } else
+    if (m === 59) {
+        return count[h] + " fifty nine ";
+    }
+
+  };
