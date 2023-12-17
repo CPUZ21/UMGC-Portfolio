a553b54b13bcf71cfe6484eae0b2ba4d37d30dc0
isaiah <itomlin801@gmail.com>
2023-12-17 17:36:12 -0500

    commit

--- submissions/capstone/routes/app.js
+++ submissions/capstone/routes/app.js
@@ -2,13 +2,30 @@ const YOUTUBE_SEARCH_URL =
 'https://www.googleapis.com/youtube/v3/search';
 const key = 'AIzaSyAwlWaOK-ZH1FnQG4187zQkjsbJQVkRP4g';
 
+var getJSON = function(url, query, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url, query, true);
+xhr.setRequestHeader('Access-Control-Allow-Origin',true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
 function getDataFromApi(searchTerm, callback) {
+  
  const query = {
   part: 'snippet',
   key: key,
   q: `${searchTerm} in:name`,
  }
-  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+  getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
 function renderResult(result) {
@@ -27,18 +44,18 @@ function displayYoutubeSearchData(data) {
  $('.js-search-results').html(results);
 }
 
-// function watchSubmit() {
- $('.js-search-form').submit(event => {
+function watchSubmit() {
+  console.log('data');
+//  $(',').submit(event => {
     console.log("works");
   event.preventDefault();
-  const queryTarget = $(event.currentTarget).find('.js-query');
-  const query = queryTarget.val();
-  queryTarget.val("");
+  const queryTarget = document.getElementById('search-value');
+  console.log(queryTarget);
+  const query = queryTarget.value;
+  queryTarget.value="";
   getDataFromApi(query,displayYoutubeSearchData );
- });
-// }
+//  });
+}
 
 
 // $(watchSubmit);
-
-
